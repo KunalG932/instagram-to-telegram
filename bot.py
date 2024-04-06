588e60424ee0dbbbaa60e817dba0c8dc1842d6cc
Gaikwad Kunal Ganpat <162322840+KunalG932@users.noreply.github.com>
2024-04-06 22:57:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,175 +5,109 @@ import socks
 import time
 from instagram_private_api import Client, errors as instaErrors
 from telethon import TelegramClient, errors
-from datetime import datetime as dt
-import datetime
-from MyClient import getCookie
-
+from datetime import datetime as dt, timedelta
 
 config = ConfigParser()
 config.read('conf.ini')
 
-# ##############################################< Instagram API setup >##############################################
-
+# Instagram API setup
 user_name = config['INSTAGRAM']['username']
 password = config['INSTAGRAM']['password']
 target_username = config['INSTAGRAM']['target_username']
-feedWaitTime = config['INSTAGRAM'].getint('feedWaitTime')
+feed_wait_time = config['INSTAGRAM'].getint('feedWaitTime')
 
-instaCookie = getCookie(user_name, password)
-instaClient = Client(user_name, password, cookie=instaCookie)
-
-# ##############################################< Telegram Client Setup >##############################################
-
+# Telegram Client Setup
 api_id = config['TELEGRAM']['api_id']
 api_hash = config['TELEGRAM']['api_hash']
 target_group = config['TELEGRAM'].getint('telegram_destination_group_id')
 session_file = '6737395815:AAEuiDpU0QL0-1pG0R4B6XzmA_Fj5SSyeCU'
 
+# Proxy Setup
+proxy_enabled = config['PROXY'].getboolean('enable')
+proxy_server = config['PROXY']['server'].encode() if proxy_enabled else '159.89.49.60'
+proxy_port = config['PROXY'].getint('port') if proxy_enabled else 31264
 
-# ##############################################< Proxy >##############################################
-try:
-    proxy_enabled = config['PROXY'].getboolean('enable')
-    proxy_server = config['PROXY']['server'].encode()
-    proxy_port = config['PROXY'].getint('port')
-except KeyError:
-    proxy_enabled = True
-    proxy_server = '159.89.49.60'
-    proxy_port = 31264
-    pass
+telegram_client = TelegramClient(session_file, api_id, api_hash, proxy=(socks.SOCKS5, proxy_server, proxy_port)) if proxy_enabled else TelegramClient(session_file, api_id, api_hash)
 
 
-# if config['proxy']['enable']:
-#     sockProxy = {
-#         "proxy_type": socks.SOCKS5,
-#         "addr": conf.SOCKS5_SERVER,
-#         "port": conf.SOCKS5_PORT,
-#         "rdns": True,
-#         "username": conf.USERNAME,
-#         "password": conf.PASSWORD
-#     }
+# Helper functions
+def create_timestamp(sec=0, mins=0, hours=0, days=0):
+    now = dt.now() - timedelta(seconds=sec, minutes=mins, hours=hours, days=days)
+    timestamp = int(time.mktime(now.timetuple()))
+    return timestamp
 
 
-if proxy_enabled:
-    proxy_server = config['PROXY']['server'].encode()  # Encoding string to bytes-like object
-    proxy_port = config['PROXY'].getint('port')
-    print(f'Using proxy server {proxy_server.decode()}:{proxy_port}')  # Decoding bytes-like object for printing
-    telegramClient = TelegramClient(session_file, api_id, api_hash, proxy=(
-        socks.SOCKS5, proxy_server, proxy_port))
-else:
-    telegramClient = TelegramClient(session_file, api_id, api_hash)
-
-
-
-# telegramClient = TelegramClient(session_file, api_id, api_hash)
-
-# ##############################################< Helper functions >##############################################
-
-
-def createTimestamp(sec=0, mins=0, hours=0, days=0):
-    now = dt.now() - datetime.timedelta(seconds=sec, minutes=mins, hours=hours, days=days)
-    ttuple = now.timetuple()
-    timestamp = time.mktime(ttuple)
-    return int(timestamp)
-
-
-def downloadImg(url):
+def download_img(url):
     if not os.path.isdir('./tmp'):
         os.mkdir('./tmp')
-    r = requests.get(url)
-    file = open('./tmp/file.jpg', 'wb')
-    file.write(r.content)
-    file.close()
-    return './tmp/file.jpg'
+    response = requests.get(url)
+    file_path = './tmp/file.jpg'
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+    return file_path
 
 
-def sendItem(filePath, text=None):
+def send_item(file_path, text=None):
     try:
-        telegramClient.loop.run_until_complete(telegramClient.send_message(target_group, text, file=filePath))
-        if os.path.exists(filePath):
-            os.remove(filePath)
+        telegram_client.loop.run_until_complete(telegram_client.send_message(target_group, text, file=file_path))
+        if os.path.exists(file_path):
+            os.remove(file_path)
         return True
     except errors.rpcerrorlist.ChatIdInvalidError:
         print('[-] Invalid Target id...')
         return False
 
-# ##############################################< Main function >##############################################
 
-
+# Main function
 def main():
     try:
-        try:
-            print('[+] Starting the telegram client...')
-            telegramClient.start()
-            print('[+] Telegram client started...')
-        except KeyboardInterrupt:
-            print('[+] Quiting bot...\n')
-            exit()
-        except errors.rpcerrorlist.ApiIdInvalidError:
-            print('[-] Could not start telegram client...')
-            print("[-] Invalid API_ID/API_HASH For Telegram...")
-            exit()
+        print('[+] Starting the Telegram client...')
+        telegram_client.start()
+        print('[+] Telegram client started...')
 
         while True:
-            print('[+] Getting instagram feeds....')
+            print('[+] Getting Instagram feeds....')
             try:
-                userfeed = instaClient.username_feed(target_username, min_timestamp=createTimestamp(sec=feedWaitTime+5))
+                user_feed = insta_client.username_feed(target_username, min_timestamp=create_timestamp(sec=feed_wait_time + 5))
             except instaErrors.ClientCheckpointRequiredError:
                 print('[-] Instagram has detected bot behaviour...')
                 print('[-] Bot is going to sleep... Please login using the browser then restart bot...')
-                try:
-                    time.sleep(6000)
-                    print('[-] Quiting bot...')
-                    exit()
-                except KeyboardInterrupt:
-                    print('[-] Quiting bot...')
-                    time.sleep(1)
-                    exit()
+                time.sleep(6000)
+                print('[-] Quitting bot...')
+                exit()
 
-            listOfItems = userfeed['items']
-            if listOfItems:
-                print('[+] New Instagram feeds found...\n[+] Sending feeds to telegram group...')
-                for item in listOfItems:
-                    if item['caption']:
-                        text = item['caption']['text']
+            list_of_items = user_feed['items']
+            if list_of_items:
+                print('[+] New Instagram feeds found...\n[+] Sending feeds to Telegram group...')
+                for item in list_of_items:
+                    text = item['caption']['text'] if 'caption' in item else None
+                    item_type = item['media_type']
+                    if item_type == 1:
+                        item_url = item['image_versions2']['candidates'][0]['url']
+                        img_path = download_img(item_url)
+                        sent = send_item(img_path, text=text)
+                    elif item_type == 2:
+                        item_url = item['video_versions'][0]['url']
+                        sent = send_item(item_url, text=text)
+                    if sent:
+                        print('[+] Feed sent...')
                     else:
-                        text = None
-                    # Item type will be 1 for image and 2 for video.
-                    itemType = item['media_type']
-                    if itemType == 1:
-                        itemUrl = item['image_versions2']['candidates'][0]['url']
-                        imgPath = downloadImg(itemUrl)
-                        sent = sendItem(imgPath, text=text)
-                        if sent:
-                            print('[+] Feed sent...')
-                            continue
-                        print('[-] Feed not sent...  Please quit the bot...')
+                        print('[-] Feed not sent... Please quit the bot...')
                         break
-                    if itemType == 2:
-                        itemUrl = item['video_versions'][0]['url']
-                        sent = sendItem(itemUrl)
-                        if sent:
-                            print('[+] Feed sent...')
-                            continue
-                        print('[-] Feed not sent...  Please quit the bot...')
-                        break
-                # print('[+] Sent all feeds to instagram group...')
             else:
                 print('[+] No new feeds found...')
-            print(f'[+] Waiting for {feedWaitTime} Seconds before checking the feed again...')
-            time.sleep(feedWaitTime)
+            print(f'[+] Waiting for {feed_wait_time} Seconds before checking the feed again...')
+            time.sleep(feed_wait_time)
     except KeyboardInterrupt:
-        print('[+] Quiting bot... Please wait...')
+        print('[+] Quitting bot... Please wait...')
         exit()
 
 
-# ##############################################< Start execution >##############################################
-
-
+# Start execution
 if __name__ == "__main__":
     print('[+] Starting the bot...')
     try:
         main()
     except KeyboardInterrupt:
-        print('[+] Quiting bot...')
+        print('[+] Quitting bot...')
         exit()
